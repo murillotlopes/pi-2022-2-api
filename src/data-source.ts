fc631187910c18bb293490479f869bb0f16f28ea
murillotlopes <murillo.tlopes@hotmail.com>
2022-10-15 15:13:54 -0300

    armazenamento em dataHeroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,13 +5,22 @@ import { config } from 'dotenv';
 
 config()
 
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD as string,
+//     database: process.env.POSTGRES_DB,
+//     logging: false,
+//     ssl: { rejectUnauthorized: false },
+//     entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
+//     migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
+// });
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD as string,
-    database: process.env.POSTGRES_DB,
+    url: process.env.DATABASE_URL,
     logging: false,
     ssl: { rejectUnauthorized: false },
     entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
